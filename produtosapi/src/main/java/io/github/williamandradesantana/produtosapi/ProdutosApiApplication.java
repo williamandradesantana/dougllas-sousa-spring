47d42be7176022c18af72a23ed40a22654a5d483
William Santana <williamandrade1058@gmail.com>
2025-09-03 16:52:17 -0300

    feat: hello world with spring boot

--- produtosapi/src/main/java/io/github/williamandradesantana/produtosapi/ProdutosApiApplication.java
+++ produtosapi/src/main/java/io/github/williamandradesantana/produtosapi/ProdutosApiApplication.java
@@ -2,9 +2,15 @@ package io.github.williamandradesantana.produtosapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ProdutosApiApplication {
+
+    @GetMapping("/helloWorld")
+    public String helloWorld() { return "Hello World!"; }
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProdutosApiApplication.class, args);
