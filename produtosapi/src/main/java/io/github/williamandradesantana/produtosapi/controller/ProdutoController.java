fb8f45183edb5d999f3912c16006ecaacd71644d
William Santana <williamandrade1058@gmail.com>
2025-09-04 13:30:23 -0300

    Obtendo os dados do produto

--- produtosapi/src/main/java/io/github/williamandradesantana/produtosapi/controller/ProdutoController.java
+++ produtosapi/src/main/java/io/github/williamandradesantana/produtosapi/controller/ProdutoController.java
@@ -2,10 +2,7 @@ package io.github.williamandradesantana.produtosapi.controller;
 
 import io.github.williamandradesantana.produtosapi.model.Produto;
 import io.github.williamandradesantana.produtosapi.repository.ProdutoRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -28,5 +25,10 @@ public class ProdutoController {
 
         produtoRepository.save(produto);
         return produto;
+    }
+
+    @GetMapping("/{id}")
+    public Produto obterPorId(@PathVariable("id") String id) {
+        return produtoRepository.findById(id).orElse(null);
     }
 }
