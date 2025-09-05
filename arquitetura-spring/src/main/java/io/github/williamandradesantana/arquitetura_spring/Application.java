a527a730c560026f4e17063d5a0ce1bee7d33328
William Santana <williamandrade1058@gmail.com>
2025-09-05 15:00:12 -0300

    arquitetura spring

---
+++ arquitetura-spring/src/main/java/io/github/williamandradesantana/arquitetura_spring/Application.java
@@ -0,0 +1,32 @@
+package io.github.williamandradesantana.arquitetura_spring;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+//        SpringApplication.run(Application.class, args);
+
+        SpringApplicationBuilder builder = new SpringApplicationBuilder(Application.class);
+        builder.run(args);
+
+        builder.bannerMode(Banner.Mode.OFF);
+
+        ConfigurableApplicationContext applicationContext = builder.context();
+//        var produtoRepository = applicationContext.getBean("produtoRepository");
+
+        builder.profiles("producao");
+//        builder.properties("spring.datasource.url=jdbc://");
+
+        ConfigurableEnvironment environment = applicationContext.getEnvironment();
+        String appliactionName = environment.getProperty("spring.application.name");
+        System.out.println(appliactionName);
+	}
+
+}
