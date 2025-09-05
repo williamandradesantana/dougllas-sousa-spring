d9dc0854cb717d7876682c306804895f663ee593
William Santana <williamandrade1058@gmail.com>
2025-09-05 15:46:24 -0300

    Testando o fluxo através do postman

--- arquitetura-spring/src/main/java/io/github/williamandradesantana/arquitetura_spring/montadora/api/TesteFabricaController.java
+++ arquitetura-spring/src/main/java/io/github/williamandradesantana/arquitetura_spring/montadora/api/TesteFabricaController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/arquitetura-spring")
+@RequestMapping("/carros")
 public class TesteFabricaController {
 
     @Autowired
