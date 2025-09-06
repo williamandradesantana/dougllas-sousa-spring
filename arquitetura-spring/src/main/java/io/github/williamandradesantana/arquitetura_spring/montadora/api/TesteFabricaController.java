79b69251d48a028e2d2c2bc16448dee3f917606f
William Santana <williamandrade1058@gmail.com>
2025-09-06 12:29:09 -0300

    Beans Primary e annotations de Qualifiers

--- arquitetura-spring/src/main/java/io/github/williamandradesantana/arquitetura_spring/montadora/api/TesteFabricaController.java
+++ arquitetura-spring/src/main/java/io/github/williamandradesantana/arquitetura_spring/montadora/api/TesteFabricaController.java
@@ -13,7 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class TesteFabricaController {
 
     @Autowired
-    @Qualifier("motorTurbo")
+//    @Qualifier("motorTurbo")
+    @Turbo
     private Motor motor;
 
     @PostMapping("/")
--- arquitetura-spring/src/main/java/io/github/williamandradesantana/arquitetura_spring/montadora/configuration/MontadoraConfiguration.java
+++ arquitetura-spring/src/main/java/io/github/williamandradesantana/arquitetura_spring/montadora/configuration/MontadoraConfiguration.java
@@ -4,11 +4,13 @@ import io.github.williamandradesantana.arquitetura_spring.montadora.Motor;
 import io.github.williamandradesantana.arquitetura_spring.montadora.TipoMotor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class MontadoraConfiguration {
 
     @Bean(name = "motorAspirado")
+    @Primary
     public Motor motorAspirado() {
         var motor = new Motor();
 
