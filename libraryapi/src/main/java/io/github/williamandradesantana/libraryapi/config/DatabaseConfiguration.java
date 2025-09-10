583db96321abd6eec79f71d46feaadc0ddf5551b
William Santana <williamandrade1058@gmail.com>
2025-09-10 20:46:15 -0300

    Como configurar um DataSource e um Pool de conexões

---
+++ libraryapi/src/main/java/io/github/williamandradesantana/libraryapi/config/DatabaseConfiguration.java
@@ -0,0 +1,51 @@
+package io.github.williamandradesantana.libraryapi.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfiguration {
+
+    @Value("${spring.datasource.url}")
+    String url;
+    @Value("${spring.datasource.username}")
+    String username;
+    @Value("${spring.datasource.password}")
+    String password;
+    @Value("${spring.datasource.driver-class-name}")
+    String driver;
+
+//    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setUrl(url);
+        ds.setUsername(username);
+        ds.setPassword(password);
+        ds.setDriverClassName(driver);
+        return ds;
+    }
+
+//    @Bean
+    public DataSource hikariDataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
+        config.setDriverClassName(driver);
+
+        config.setMaximumPoolSize(10); // Máximo de conexões liberadas
+        config.setMinimumIdle(1); // Tamanho inicial do pool
+        config.setPoolName("library-pool");
+        config.setMaximumPoolSize(600000); // 10 minutos
+        config.setConnectionTimeout(100000); // timeout para conseguir conexão
+        config.setConnectionTestQuery("select 1"); // query teste
+
+        return new HikariDataSource(config);
+    }
+}
