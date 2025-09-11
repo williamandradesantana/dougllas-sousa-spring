0f88bbc8dece042bd72d4ff9cf76ff00419a7d17
William Santana <williamandrade1058@gmail.com>
2025-09-11 13:10:24 -0300

    Como mapear entidades JPA

---
+++ libraryapi/src/main/java/io/github/williamandradesantana/libraryapi/model/Author.java
@@ -0,0 +1,25 @@
+package io.github.williamandradesantana.libraryapi.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "author", schema = "public")
+@Getter
+@Setter
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private UUID id;
+    @Column(name = "name", length = 100, nullable = false)
+    private String name;
+    @Column(name = "date_of_birth", nullable = false)
+    private LocalDate dateOfBirth;
+    @Column(name = "nationality", length = 50, nullable = false)
+    private String nationality;
+}
