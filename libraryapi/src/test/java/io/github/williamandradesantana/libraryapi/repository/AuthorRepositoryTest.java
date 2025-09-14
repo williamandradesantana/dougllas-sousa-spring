f8c0ded22f8a588cb3c03627017003bcff05ed48
William Santana <williamandrade1058@gmail.com>
2025-09-14 15:21:16 -0300

    Aplicando testes no repository

---
+++ libraryapi/src/test/java/io/github/williamandradesantana/libraryapi/repository/AuthorRepositoryTest.java
@@ -0,0 +1,11 @@
+package io.github.williamandradesantana.libraryapi.repository;
+
+import io.github.williamandradesantana.libraryapi.repositories.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class AuthorRepositotyTest {
+
+    @Autowired
+    private AuthorRepository
+    public void saveTest(){}
+}
