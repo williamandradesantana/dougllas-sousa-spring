d1b0232f7f731b4bd9d12cb696f9950bc218fd79
William Santana <williamandrade1058@gmail.com>
2025-09-24 17:25:15 -0300

    Adicionando Auditoria JPA nas entidades

--- libraryapi/src/main/java/io/github/williamandradesantana/libraryapi/model/Author.java
+++ libraryapi/src/main/java/io/github/williamandradesantana/libraryapi/model/Author.java
@@ -3,8 +3,12 @@ package io.github.williamandradesantana.libraryapi.model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -12,17 +16,32 @@ import java.util.UUID;
 @Table(name = "tb_authors", schema = "public")
 @Getter
 @Setter
+@EntityListeners(AuditingEntityListener.class)
 public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "id")
     private UUID id;
+
     @Column(name = "name", length = 100, nullable = false)
     private String name;
+
     @Column(name = "date_of_birth", nullable = false)
     private LocalDate dateOfBirth;
+
     @Column(name = "nationality", length = 50, nullable = false)
     private String nationality;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "user_id")
+    private UUID userId;
 
     @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Book> books;
