99ad3d6fbe85aca8d48b56754e4de2e24f76458d
William Santana <williamandrade1058@gmail.com>
2025-09-24 16:02:52 -0300

    Iniciando a codificação da API

---
+++ libraryapi/src/main/java/io/github/williamandradesantana/libraryapi/controller/AuthorController.java
@@ -0,0 +1,9 @@
+package io.github.williamandradesantana.libraryapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/authors")
+public class AuthorController {
+}
