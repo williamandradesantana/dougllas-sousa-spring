151a15eef43391b5f7c3995931fc96962fe73825
William Santana <williamandrade1058@gmail.com>
2025-09-29 20:45:48 -0300

    Explorando tipos de validação no Spring

--- libraryapi/src/main/java/io/github/williamandradesantana/libraryapi/controller/dto/AuthorDTO.java
+++ libraryapi/src/main/java/io/github/williamandradesantana/libraryapi/controller/dto/AuthorDTO.java
@@ -3,6 +3,8 @@ package io.github.williamandradesantana.libraryapi.controller.dto;
 import io.github.williamandradesantana.libraryapi.model.Author;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 import java.util.UUID;
@@ -10,10 +12,13 @@ import java.util.UUID;
 public record AuthorDTO(
         UUID id,
         @NotBlank(message = "The name is required!")
+        @Size(min = 2, max = 100, message = "The name must be between 3 and 100 chars!")
         String name,
         @NotNull(message = "The date of birth is required!")
+        @PastOrPresent(message = "Date of birth cannot be a future date")
         LocalDate dateOfBirth,
         @NotBlank(message = "The nationality is required!")
+        @Size(min = 2, max = 100, message = "The name must be between 3 and 50 chars!")
         String nationality
 ) {
     public Author mapAuthor() {
