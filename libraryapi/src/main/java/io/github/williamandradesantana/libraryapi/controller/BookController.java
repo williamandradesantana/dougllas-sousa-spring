7868b6563fd1127ee8f3f19b492c19d436fcf3e6
William Santana <williamandrade1058@gmail.com>
2025-09-30 09:50:17 -0300

    Endpoint para cadastro de livro com Bean Validation

--- libraryapi/src/main/java/io/github/williamandradesantana/libraryapi/controller/BookController.java
+++ libraryapi/src/main/java/io/github/williamandradesantana/libraryapi/controller/BookController.java
@@ -1,7 +1,14 @@
 package io.github.williamandradesantana.libraryapi.controller;
 
+import io.github.williamandradesantana.libraryapi.controller.dto.BookRegisterDTO;
+import io.github.williamandradesantana.libraryapi.controller.dto.ResponseError;
+import io.github.williamandradesantana.libraryapi.exceptions.DuplicateRegisterException;
 import io.github.williamandradesantana.libraryapi.services.BookService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +18,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookController {
 
     private final BookService bookService;
+
+    @PostMapping("/")
+    public ResponseEntity<Object> create(@Valid @RequestBody BookRegisterDTO dto) {
+        try {
+            // mapear dto para entidade
+            // enviar a entidade para o service, validar e salvar na base
+            // criar url para acesso dos dados do livro
+            // retornar código created com header location
+            return ResponseEntity.ok().body(dto);
+        } catch (DuplicateRegisterException e) {
+            var error = ResponseError.conflictError("ISBN duplicated!");
+            return ResponseEntity.status(error.status()).body(error.message());
+        }
+
+    }
 }
--- libraryapi/src/main/java/io/github/williamandradesantana/libraryapi/controller/dto/BookRegisterDTO.java
+++ libraryapi/src/main/java/io/github/williamandradesantana/libraryapi/controller/dto/BookRegisterDTO.java
@@ -1,17 +1,27 @@
 package io.github.williamandradesantana.libraryapi.controller.dto;
 
 import io.github.williamandradesantana.libraryapi.model.enums.Gender;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import org.hibernate.validator.constraints.ISBN;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
 public record BookRegisterDTO(
+        @ISBN
+        @NotBlank(message = "The isbn is required!")
         String isbn,
+        @NotBlank(message = "The title is required!")
         String title,
+        @NotNull(message = "The publish date is required!")
+        @PastOrPresent(message = "Publish date cannot be a future date")
         LocalDate publishDate,
         Gender gender,
         BigDecimal price,
+        @NotNull(message = "The author id is required!")
         UUID authorId
 ) {
 }
