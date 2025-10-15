699de978e2b712c444a4fdeb0bcee26d7d23b50d
William Santana <williamandrade1058@gmail.com>
2025-10-15 18:47:13 -0300

    Criando a estrutura de usuários no banco de dados

---
+++ libraryapi/src/main/java/io/github/williamandradesantana/libraryapi/model/User.java
@@ -0,0 +1,30 @@
+package io.github.williamandradesantana.libraryapi.model;
+
+import io.hypersistence.utils.hibernate.type.array.ListArrayType;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.Type;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_users")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false)
+    private UUID id;
+
+    @Column(name = "login", length = 20, nullable = false, unique = true)
+    private String login;
+
+    @Column(name = "password", length = 300, nullable = false)
+    private String password;
+
+    @Type(ListArrayType.class)
+    @Column(name = "roles", columnDefinition = "varchar[]")
+    private List<String> roles;
+}
