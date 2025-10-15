3f368acb516f9eb2a5fd4e107b0a440c271e3ff9
William Santana <williamandrade1058@gmail.com>
2025-10-15 18:30:20 -0300

    Trabalhando com roles de usuario

--- libraryapi/src/main/java/io/github/williamandradesantana/libraryapi/config/SecurityConfiguration.java
+++ libraryapi/src/main/java/io/github/williamandradesantana/libraryapi/config/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package io.github.williamandradesantana.libraryapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -24,10 +25,22 @@ public class SecurityConfiguration {
                 .csrf(AbstractHttpConfigurer::disable)
 //                .formLogin(Customizer.withDefaults())
                 .formLogin(configurer -> {
-                    configurer.loginPage("/login").permitAll();
+                    configurer.loginPage("/login");
                 })
                 .httpBasic(Customizer.withDefaults())
                 .authorizeHttpRequests(authorize -> {
+
+
+//                    authorize.requestMatchers("/login").permitAll();
+//                    authorize.requestMatchers(HttpMethod.POST, "/authors/**").hasAuthority("REGISTER_AUTHOR");
+//                    authorize.requestMatchers(HttpMethod.POST, "/authors/**").hasRole("ADMIN");
+//                    authorize.requestMatchers(HttpMethod.DELETE,"/authors/**").hasRole("ADMIN");
+//                    authorize.requestMatchers(HttpMethod.PUT,"/authors/**").hasRole("ADMIN");
+//                    authorize.requestMatchers(HttpMethod.GET,"/authors/**").hasAnyRole("ADMIN", "USER");
+                    authorize.requestMatchers("/login/**").permitAll();
+                    authorize.requestMatchers("/authors/**").hasRole("ADMIN");
+                    authorize.requestMatchers("/books/**").hasAnyRole("USER", "ADMIN");
+
                     authorize.anyRequest().authenticated();
                 })
                 .build();
