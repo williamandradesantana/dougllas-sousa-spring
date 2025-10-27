49465c49b32efe71fe00c48d8bd559f23e1528e1
William Santana <williamandrade1058@gmail.com>
2025-10-27 09:03:20 -0300

    Eliminando o prefixo ROLE do security

--- libraryapi/src/main/java/io/github/williamandradesantana/libraryapi/config/SecurityConfiguration.java
+++ libraryapi/src/main/java/io/github/williamandradesantana/libraryapi/config/SecurityConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.core.GrantedAuthorityDefaults;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -59,5 +60,10 @@ public class SecurityConfiguration {
 //                .build();
 //        return new InMemoryUserDetailsManager(user, admin);
         return new CustomUserDetailsService(userServices);
+    }
+
+    @Bean
+    public GrantedAuthorityDefaults grantedAuthorityDefaults() {
+        return new GrantedAuthorityDefaults(""); // ignora o ROLE_
     }
 }
