ea4362a75469748a4ce86147e40850f2b5cecf65
William Santana <williamandrade1058@gmail.com>
2025-11-05 09:31:59 -0300

    Configurações globais para os clients

---
+++ libraryapi/src/main/java/io/github/williamandradesantana/libraryapi/config/AuthorizationServerConfiguration.java
@@ -0,0 +1,28 @@
+package io.github.williamandradesantana.libraryapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
+
+import java.time.Duration;
+
+@Configuration
+@EnableWebSecurity
+public class AuthorizationServerConfiguration {
+
+    @Bean
+    public TokenSettings tokenSettings() {
+        return TokenSettings.builder()
+                .accessTokenFormat(OAuth2TokenFormat.SELF_CONTAINED)
+                .accessTokenTimeToLive(Duration.ofMinutes(60))
+                .build();
+    }
+
+    @Bean
+    public ClientSettings clientSettings() {
+        return ClientSettings.builder().requireAuthorizationConsent(false).build();
+    }
+}
--- libraryapi/src/main/java/io/github/williamandradesantana/libraryapi/security/CustomRegisteredClientRepository.java
+++ libraryapi/src/main/java/io/github/williamandradesantana/libraryapi/security/CustomRegisteredClientRepository.java
@@ -6,6 +6,8 @@ import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,6 +15,8 @@ import org.springframework.stereotype.Component;
 public class CustomRegisteredClientRepository implements RegisteredClientRepository {
 
     private final ClientService clientService;
+    private final TokenSettings tokenSettings;
+    private final ClientSettings clientSettings;
 
     @Override
     public void save(RegisteredClient registeredClient) {}
@@ -38,6 +42,8 @@ public class CustomRegisteredClientRepository implements RegisteredClientReposit
                 .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
                 .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .tokenSettings(tokenSettings)
+                .clientSettings(clientSettings)
                 .build();
     }
 }
