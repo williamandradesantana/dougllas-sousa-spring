2d32e7495bc10b3146fb7eded9abd5ca66b87e31
William Santana <williamandrade1058@gmail.com>
2025-11-11 11:01:46 -0300

    Se autenticando com Authorization Code grant type

--- libraryapi/src/main/java/io/github/williamandradesantana/libraryapi/controller/LoginViewController.java
+++ libraryapi/src/main/java/io/github/williamandradesantana/libraryapi/controller/LoginViewController.java
@@ -4,6 +4,7 @@ import io.github.williamandradesantana.libraryapi.security.CustomAuthentication;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -21,5 +22,11 @@ public class LoginViewController {
             return "Hello World! " + customAuthentication.getName();
         }
         return "Hello World! " + authentication.getName();
+    }
+
+    @GetMapping("/authorized")
+    @ResponseBody
+    public String getAuthorizationCode(@RequestParam("code") String code) {
+        return "Your authorization code: " + code;
     }
 }
