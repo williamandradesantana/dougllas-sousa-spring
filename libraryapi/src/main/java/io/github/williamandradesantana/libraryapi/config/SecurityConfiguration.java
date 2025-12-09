70818d52f40696b42f41dca003dfffe13c1162b3
William Santana <williamandrade1058@gmail.com>
2025-12-09 19:36:20 -0300

    Habilitando e testando o Swagger Open API no projeto

--- libraryapi/src/main/java/io/github/williamandradesantana/libraryapi/config/SecurityConfiguration.java
+++ libraryapi/src/main/java/io/github/williamandradesantana/libraryapi/config/SecurityConfiguration.java
@@ -9,12 +9,15 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.core.GrantedAuthorityDefaults;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.oauth2.server.resource.web.authentication.BearerTokenAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -47,6 +50,20 @@ public class SecurityConfiguration {
                 .oauth2ResourceServer(oauth2RS -> oauth2RS.jwt(Customizer.withDefaults()))
                 .addFilterAfter(jwtCustomAuthenticationFilter, BearerTokenAuthenticationFilter.class)
                 .build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> {
+          web.ignoring().requestMatchers(
+                  "/v2/api-docs/**",
+                  "/v3/api-docs/**",
+                  "/swagger/resources/**",
+                  "/swagger-ui.html",
+                  "/swagger-ui/**",
+                  "/webjars/**"
+          );
+        };
     }
 
     @Bean
