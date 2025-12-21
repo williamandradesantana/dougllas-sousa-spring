e80f43626dd7922bb30e05baef06321c6aadbed7
William Santana <williamandrade1058@gmail.com>
2025-12-20 22:33:03 -0300

    Utilizando o Actuator para obter métricas da aplicação

--- libraryapi/src/main/java/io/github/williamandradesantana/libraryapi/config/SecurityConfiguration.java
+++ libraryapi/src/main/java/io/github/williamandradesantana/libraryapi/config/SecurityConfiguration.java
@@ -59,7 +59,8 @@ public class SecurityConfiguration {
                   "/swagger/resources/**",
                   "/swagger-ui.html",
                   "/swagger-ui/**",
-                  "/webjars/**"
+                  "/webjars/**",
+                  "/actuator/**"
           );
         };
     }
