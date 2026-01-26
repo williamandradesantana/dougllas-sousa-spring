1556f70ecc7b80b121fb55b7f34ca40c27058a15
William Santana <williamandrade1058@gmail.com>
2026-01-26 11:25:51 -0300

    Testando Exceptions e finalizando o desafio

--- rentalCompany/src/test/java/io/github/williamandradesatana/rentalCompany/modules/reserve/entity/ReserveTest.java
+++ rentalCompany/src/test/java/io/github/williamandradesatana/rentalCompany/modules/reserve/entity/ReserveTest.java
@@ -34,4 +34,26 @@ public class ReserveTest {
         assertNotNull(reserve);
         assertThat(reserve.getClient().getName()).isEqualTo("Jon");
     }
+
+    @Test
+    @DisplayName("It should throw an error if you create it when adding negative days to the reservation.")
+    void itNeedsToFailWhenEnteringNegativeDays() {
+
+        // JUnit
+        assertThrows(InvalidReservationException.class, () -> new Reserve(client, car, 0));
+        assertDoesNotThrow(() -> new Reserve(client, car, 1));
+
+        // AssertJ
+        var error = catchThrowable(() -> new Reserve(client, car, -1));
+        assertThat(error).isInstanceOf(InvalidReservationException.class).hasMessage("Invalid reservation");
+    }
+
+    @Test
+    void mustBeCalculateTotalRent() {
+        var reserve = new Reserve(client, car, 6);
+
+        var total = reserve.calculateReservation();
+
+        assertThat(total).isEqualTo(550.0);
+    }
 }
